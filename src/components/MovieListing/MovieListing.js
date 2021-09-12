31e15a14a33768a1a540d0263360ec61fbffb01a
Rezaei <ali.rezaei0021@gmail.com>
2021-09-12 14:08:52 +0430

    create cart

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -2,18 +2,19 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { getAllMovies } from "../../features/MovieSlice/MovieSlice";
 import MovieCard from "../MovieCard/MovieCard";
-import "./MovieListing.scss"
+import "./MovieListing.scss";
+
 const MovieListing = () => {
+
   const movies = useSelector(getAllMovies);
-console.log(movies);
+
   let renderMovies = "";
 
   renderMovies =
-    movies.Response === "true" ? (
-      movies.Search.map((movie, index) =>
-          <MovieCard key={index} data={movie} />
-        
-      )
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
     ) : (
       <div className="movies-error">
         <h3>{movies.Error}</h3>
@@ -21,8 +22,8 @@ console.log(movies);
     );
 
   return (
-    <div className="movie-wraper">
-      <div className="moveie-list">
+    <div className="movie-wrapper">
+      <div className="movie-list">
         <h2>Movies</h2>
         <div className="movei-container">{renderMovies}</div>
       </div>
